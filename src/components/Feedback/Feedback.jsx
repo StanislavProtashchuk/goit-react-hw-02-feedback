7f1a543dab95721f3b59a6c6119213074fa4dd7f
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-05-05 18:29:53 +0300

    startWotkingThreeMethods

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,18 +2,43 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class Counter extends React.Component{
+
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    handleGood = () => {
+        this.setState((prevState) => ({
+            good: prevState.good + 1,
+        }));
+    };
+
+        handleNeutral = () => {
+        this.setState((prevState) => ({
+            neutral: prevState.neutral + 1,
+        }));
+    };
+
+            handleBad = () => {
+        this.setState((prevState) => ({
+            bad: prevState.bad + 1,
+        }));
+    };
+
     render() {
         return (
         <div>
         <h2>Please leave feedback</h2>
-        <button>Good</button>
-        <button>Neutral</button>
-            <button>Bad</button>
+        <button type="button" onClick={this.handleGood}>Good</button>
+        <button type="button" onClick={this.handleNeutral}>Neutral</button>
+            <button type="button" onClick={this.handleBad}>Bad</button>
             <h3>Statistics</h3>
             <ul>
-                <li>Good: <span>0</span> </li>
-                <li>Neutral: <span>0</span> </li>
-                <li>Bad: <span>0</span> </li>
+                    <li>Good: <span>{this.state.good}</span> </li>
+                <li>Neutral: <span>{this.state.neutral}</span> </li>
+                <li>Bad: <span>{this.state.bad}</span> </li>
             </ul>
             </div>
     )
@@ -21,20 +46,3 @@ class Counter extends React.Component{
 }
 
 export default Counter;
-
-// export default function Feedback() {
-//     return (
-//         <div>
-//         <h2>Please leave feedback</h2>
-//         <button>Good</button>
-//         <button>Neutral</button>
-//             <button>Bad</button>
-//             <h3>Statistics</h3>
-//             <ul>
-//                 <li>Good: <span>0</span> </li>
-//                 <li>Neutral: <span>0</span> </li>
-//                 <li>Bad: <span>0</span> </li>
-//             </ul>
-//             </div>
-//     )
-// }
